995ec731d9858ee2eda045d55443060f6bb42111
kakisoft <kakisttab@gmail.com>
2017-10-10 20:10:32 +0900

    edit string

--- string.rb
+++ string.rb
@@ -4,8 +4,18 @@ puts 'hello\no worl\td'  #=> hello\no worl\td  （改行、タブは表現され
 puts "price #{3000 * 4}" #=> price 12000
 puts 'price #{3000 * 4}' #=> price #{3000 * 4}
 
-name = "kaki"
-puts "hello #{name}" #=> hello kaki
+name1 = "kaki"
+puts "hello #{name1}" #=> hello kaki
 
 puts "hello " + "world" #=> hello world
 puts "hello" * 10       #=> helloを10回表示
+
+name2 = "yamada"
+puts name2.upcase   #=> YAMADA
+puts name2          #=> yamada
+puts name2.upcase!  #=> YAMADA（破壊的なメソッド　※元のオブジェクトを変える）
+puts name2          #=> YAMADA
+
+name3 = "sawada"
+p name3.empty?        #=> false 
+p name3.include?("s") #=> true
