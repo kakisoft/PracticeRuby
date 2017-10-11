a5a2d5e8bac35571fa314dcf980e59eee5ca494d
kakisoft <kakisttab@gmail.com>
2017-10-12 08:04:24 +0900

    edit roop

--- roop.rb
+++ roop.rb
@@ -1,3 +1,34 @@
+#===========================
+#         each
+#===========================
+(15..20).each do |i|
+  p i
+end
+
+["red","blue","yellow"].each do |color|
+  p color
+end
+    
+{takeda:200, uesugi:400}.each do |name, score|
+  puts "#{name}: #{score}"
+end
+
+
+{takeda:200, uesugi:400}.each {|name, score| puts "#{name}: #{score}"}
+  
+
+#===========================
+#         times
+#===========================
+10.times do |i|
+  puts "#{i} Hi!"
+end
+
+10.times { |i| puts "#{i} Hi!"}  # do, end を{}に変えて、1行にまとめたバージョン。
+
+#===========================
+#         while
+#===========================
 i = 0 
 while i < 10 do
   puts "#{i}: hello"
@@ -5,8 +36,19 @@ while i < 10 do
 end
 
 
-10.times do |i|
-  puts "#{i} Hi!"
+#===========================
+#         for
+#===========================
+for i in 15..20 do  # doは省略可
+  p i  # 15～20まで表示
 end
 
-10.times { |i| puts "#{i} Hi!"}  # do, end を{}に変えて、1行にまとめたバージョン。
+for color in ["red","blue","yellow"]
+  p color
+end
+  
+for name, score in {takeda:200, uesugi:400}
+  puts "#{name}: #{score}"
+end
+  
+
